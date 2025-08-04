deafa6a1aff143288f865e70a580679fa50da1ea
HaiderMarhoon <haidooer2003@gmail.com>
2025-08-04 11:03:42 +0300

    show

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 // src/App.jsx
-
+import { useEffect, useState } from 'react';
+import * as petService from './services/petService'
+import PetList from './components/PetList';
 const App = () => {
-  return <h1>Hello world!</h1>;
+
+  const [pets, setPets] = useState([])
+  useEffect(() => {
+    const fetchPets = async () => {
+      try {
+        const fetchedPets = await petService.index()
+        setPets(fetchedPets)
+      }
+      catch (err) {
+        console.log(err)
+      }
+    }
+    fetchPets()
+
+  }, [])
+  petService.index()
+
+  return (
+    <>
+    <PetList pets = {pets}/>
+    </>
+  )
+
 };
 
 export default App;
